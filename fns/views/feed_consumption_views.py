19570a218d9e90f9a59bd5dc977e8e8b90f3ecf4
alecompuny2020-source <@le.compuny2020@gmail.com>
2026-05-31 20:51:32 +0300

    food and nutrition views

---
+++ fns/views/feed_consumption_views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from common.mixins import BaseEnterpriseViewSet
+from common.permissions.base import EnterpriseObjectLevelPermissionMixin
+from fns.models import FeedConsumption
+from fns.serializers import FeedConsumptionSerializer
+
+# Create your views here.
+
+class FeedTypeViewSet(EnterpriseObjectLevelPermissionMixin, BaseEnterpriseViewSet):
+    queryset = FeedConsumption.objects.select_related('batch', 'feed_type').all()
+    serializer_class = FeedConsumptionSerializer
---
+++ fns/views/feed_inventory_views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from common.mixins import BaseEnterpriseViewSet
+from common.permissions.base import EnterpriseObjectLevelPermissionMixin
+from fns.models import FeedInventory
+from fns.serializers import FeedInventorySerializer
+
+# Create your views here.
+
+class FeedInventoryViewSet(EnterpriseObjectLevelPermissionMixin, BaseEnterpriseViewSet):
+    queryset = FeedInventory.objects.select_related('feed_type').all()
+    serializer_class = FeedInventorySerializer
