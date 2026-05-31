c9acfadbed922d8dd40f3a8a185cf31063273c25
alecompuny2020-source <@le.compuny2020@gmail.com>
2026-05-31 22:34:06 +0300

    modigied

--- ppms/serializers/processing_serializers.py
+++ ppms/serializers/processing_serializers.py
@@ -9,3 +9,31 @@ class ProcessingPlantSerializers(BaseEnterpriseAuditSerializer):
             'name', 'location', 'plant_manager', 'plant_config', 'certification_info',
             'is_active', 'created_by', 'updated_by', 'created_on', 'updated_on'
         ]
+
+
+
+class ProcessingSessionSerializers(BaseEnterpriseAuditSerializer):
+    class Meta:
+        model = ProcessingSession
+        fields = [
+            'plant',
+        'source_batch',
+        'assigned_workers',
+        'birds_processed',
+        'live_birds_count',
+        'total_live_weight',
+        'total_dressed_weight',
+        'slaughter_data',
+        'environmental_logs',
+        'start_time',
+        'end_time',
+        'yield_percentage',
+        'temperature_recorder',
+        'rejected_birds',
+        'bird_loss_count',
+        'slaughter_shrinkage_kg',
+'created_by',
+'updated_by',
+'created_on',
+'updated_on'
+        ]
