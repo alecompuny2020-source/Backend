1db0313235ab6191c57e0b5445902b44c0936033
alecompuny2020-source <@le.compuny2020@gmail.com>
2026-06-03 07:05:31 +0300

    updated ipss routes

--- ipss/urls.py
+++ ipss/urls.py
@@ -1,13 +1,19 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+
 from . import views as v
 
 router = DefaultRouter()
 
-# router.register(r"plants", v.ProcessingPlantViewSet)
-# router.register(r"processing-sessions", v.ProcessingSessionViewSet, basename = 'sessions')
+router.register(r"warehouses", v.WarehouseLocationViewSet)
+router.register(r"zones", v.ZoneViewSet)
+router.register(r"storage-units", v.StorageUnitViewSet)
+router.register(r"products", v.ProductViewSet)
+router.register(r"product-variants", v.ProductVariantViewSet, basename = "variants")
+router.register(r"product-stocks", v.ProductStockViewSet, basename = "stock")
+router.register(r"product-movements", v.StockMovementViewSet, basename = "movement")
 
 urlpatterns = [
-    # path("", include(router.urls)),
+    path("", include(router.urls)),
 ]
